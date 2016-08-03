64dccc9ef43fd816753cd235bfad4a060d4a06c6
Asofi <asofi94@gmail.com>
2016-08-03 17:50:06 +0300

    Рандомное заполнение ручками.

--- Safe2/Form1.Designer.cs
+++ Safe2/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.btnStart3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -67,11 +68,22 @@
             this.btnStart3.UseVisualStyleBackColor = true;
             this.btnStart3.Click += new System.EventHandler(this.btnStart3_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Flip!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 339);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnStart3);
             this.Controls.Add(this.numericUpDown1);
             this.Name = "Form1";
@@ -85,6 +97,7 @@
         #endregion
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button btnStart3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Safe2/Form1.cs
+++ Safe2/Form1.cs
@@ -12,6 +12,10 @@ namespace Safe2
 {
     public partial class Form1 : Form
     {
+        private Image[] levers = new Image[2];
+        Random rnd = new Random();
+        private bool started = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,36 +29,54 @@ namespace Safe2
                 {
                     Button btn = new Button();
 
+                    int ID = rnd.Next(0, 2);
                     btn.Width = 40;
                     btn.Height = 40;
                     btn.Name = "Btn" + i.ToString() + j.ToString();
                     btn.Location = new Point(i * btn.Width, j * btn.Height);
+                    btn.BackgroundImage = levers[ID];
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
                     this.Controls.Add(btn);
-                    //Console.WriteLine("Button added" + btn.Name);
+                    Console.WriteLine("Image " + ID);
                 }
+        }
+
+        private void flip (Button btn, int ID)
+        {
+            btn.BackgroundImage = levers[1 - ID];
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            levers[0] = Image.FromFile("Resources/Vert.png");
+            levers[1] = Image.FromFile("Resources/Hor.png");
 
-        }
-
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("4x4");
-            CreateButtons(4);
-        }
-
-        private void btnStart2_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("5x5");
-            CreateButtons(5);
         }
 
         private void btnStart3_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Сторона: " + (int)numericUpDown1.Value);
-            CreateButtons((int)numericUpDown1.Value);
+            if (!started)
+            {
+                Console.WriteLine("Сторона: " + (int)numericUpDown1.Value);
+                CreateButtons((int)numericUpDown1.Value);
+                started = true;
+                btnStart3.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void flipper(int fieldSize)
+        {
+            for (int i = 0; i < fieldSize; i++)
+                for (int j = 0; j < fieldSize; j++)
+                {
+                  //  flip();
+                }
         }
     }
 }
