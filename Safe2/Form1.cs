6ae041e2533a1b37a78440c38da0b93a5d8cb80e
Asofi <asofi94@gmail.com>
2016-08-03 22:00:07 +0300

    Работает переключение крестом.

--- Safe2/Form1.cs
+++ Safe2/Form1.cs
@@ -12,6 +12,7 @@ namespace Safe2
 {
     public partial class Form1 : Form
     {
+        private Button[,] buttons;
         private Image[] levers = new Image[2];
         Random rnd = new Random();
         private bool started = false;
@@ -24,34 +25,56 @@ namespace Safe2
         private void CreateButtons(int fieldSize)
         {
             //Creating field of game
+            buttons = new Button[fieldSize, fieldSize];
             for (int i = 0; i < fieldSize; i++)
                 for (int j = 0; j < fieldSize; j++)
                 {
-                    Button btn = new Button();
+                    buttons[i,j] = new Button();
 
-                    btn.Tag = rnd.Next(0, 2);
-                    int ID = (int)btn.Tag;
-                    btn.Width = 60;
-                    btn.Height = 60;
-                    btn.Name = "Btn " + (i+1) + " " + (j+1);
-                    btn.Location = new Point(i * btn.Width, j * btn.Height);
-                    btn.BackgroundImage = levers[ID];
-                    btn.FlatStyle = FlatStyle.Flat;
-                    btn.BackgroundImageLayout = ImageLayout.Stretch;
-                    btn.Click += new EventHandler(btn_Click);
-                    this.Controls.Add(btn);
+                    buttons[i, j].Tag = rnd.Next(0, 2);
+                    int ID = (int)buttons[i, j].Tag;
+                    buttons[i, j].Location = new Point(i * 50, j * 50);
+                    buttons[i, j].Size = new Size(50, 50);
+                    buttons[i, j].BackgroundImage = levers[ID];
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                    buttons[i, j].MouseClick += new MouseEventHandler(btn_MouseClick);
+                    this.Controls.Add(buttons[i, j]);
+                    /*buttons[i, j].Width = 60;
+                    buttons[i, j].Height = 60;
+                    buttons[i, j].Name = "Btn " + (i+1) + " " + (j+1);
+                    buttons[i, j].Location = new Point(i * buttons[i, j].Width, j * buttons[i,j].Height);
+                    buttons[i, j].BackgroundImage = levers[ID];
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                    buttons[i, j].Click += new EventHandler(btn_Click);
+                    this.Controls.Add(buttons[i, j]);*/
                     Console.WriteLine("Image " + ID);
                 }
         }
 
-        void btn_Click(object sender, EventArgs e)
+        void btn_MouseClick(object sender, MouseEventArgs e)
         {
-            flip((Button)sender);
-            //MessageBox.Show("Нажата кнопка " + ((Button)sender).Name);
+            int j = this.PointToClient(Cursor.Position).Y / 50;
+            int i = this.PointToClient(Cursor.Position).X / 50;
+            int countI;
+            int countJ;
+            for (countI = 0; countI < (int)numericUpDown1.Value; countI++)
+            {
+                flip(buttons[countI, j]);
+                Console.WriteLine("Flipped");
+            }
+            for (countJ =0; countJ < (int)numericUpDown1.Value; countJ++)
+            {
+                if(countJ!=j)
+                flip(buttons[i, countJ]);
+                Console.WriteLine("Flipped");
+            }
+
         }
 
         private void flip (Button btn)
-        {
+        {   
             int newID = 1 - (int)btn.Tag;
             btn.BackgroundImage = levers[newID];
             btn.Tag = newID;
