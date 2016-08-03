426532b39c1a8a1a606205c8e7992da41edc4875
Asofi <asofi94@gmail.com>
2016-08-03 16:00:46 +0300

    Есть создание матрицы кнопок и выбор ее размера.

---
+++ Safe2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Safe2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnStart3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(417, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // btnStart3
+            // 
+            this.btnStart3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart3.Location = new System.Drawing.Point(461, 41);
+            this.btnStart3.Name = "btnStart3";
+            this.btnStart3.Size = new System.Drawing.Size(75, 23);
+            this.btnStart3.TabIndex = 3;
+            this.btnStart3.Text = "Start";
+            this.btnStart3.UseVisualStyleBackColor = true;
+            this.btnStart3.Click += new System.EventHandler(this.btnStart3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 339);
+            this.Controls.Add(this.btnStart3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnStart3;
+    }
+}
+
---
+++ Safe2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Safe2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButtons(int fieldSize)
+        {
+            //Creating field of game
+            for (int i = 0; i < fieldSize; i++)
+                for (int j = 0; j < fieldSize; j++)
+                {
+                    Button btn = new Button();
+
+                    btn.Width = 40;
+                    btn.Height = 40;
+                    btn.Name = "Btn" + i.ToString() + j.ToString();
+                    btn.Location = new Point(i * btn.Width, j * btn.Height);
+                    this.Controls.Add(btn);
+                    //Console.WriteLine("Button added" + btn.Name);
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4x4");
+            CreateButtons(4);
+        }
+
+        private void btnStart2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("5x5");
+            CreateButtons(5);
+        }
+
+        private void btnStart3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Сторона: " + (int)numericUpDown1.Value);
+            CreateButtons((int)numericUpDown1.Value);
+        }
+    }
+}
