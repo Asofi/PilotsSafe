ccac9ff08278ba41f236731fda46eecc26939914
Asofi <asofi94@gmail.com>
2016-08-04 15:05:20 +0300

    Немного подредактировал код. Проверка победы не работает.

--- Safe2/Form1.cs
+++ Safe2/Form1.cs
@@ -13,19 +13,30 @@ namespace Safe2
     public partial class Form1 : Form
     {
         private Button[,] buttons;
+        private int[,] win1, win0, current;
         private Image[] levers = new Image[2];
+
+        private int fieldSize;
         Random rnd = new Random();
         private bool started = false;
+        private bool won = false;
 
         public Form1()
         {
             InitializeComponent();
+            levers[0] = Image.FromFile("Resources/Vert.png");
+            levers[1] = Image.FromFile("Resources/Hor.png");
         }
 
-        private void CreateButtons(int fieldSize)
+        private void CreateButtons()
         {
-            //Creating field of game
+            fieldSize = (int)numericUpDown1.Value;
             buttons = new Button[fieldSize, fieldSize];
+            //Win conditions
+            win1 = new int[fieldSize, fieldSize];
+            win0 = new int[fieldSize, fieldSize];
+            current = new int[fieldSize, fieldSize];
+            //Creating field of game
             for (int i = 0; i < fieldSize; i++)
                 for (int j = 0; j < fieldSize; j++)
                 {
@@ -40,16 +51,8 @@ namespace Safe2
                     buttons[i, j].BackgroundImageLayout = ImageLayout.Stretch;
                     buttons[i, j].MouseClick += new MouseEventHandler(btn_MouseClick);
                     this.Controls.Add(buttons[i, j]);
-                    /*buttons[i, j].Width = 60;
-                    buttons[i, j].Height = 60;
-                    buttons[i, j].Name = "Btn " + (i+1) + " " + (j+1);
-                    buttons[i, j].Location = new Point(i * buttons[i, j].Width, j * buttons[i,j].Height);
-                    buttons[i, j].BackgroundImage = levers[ID];
-                    buttons[i, j].FlatStyle = FlatStyle.Flat;
-                    buttons[i, j].BackgroundImageLayout = ImageLayout.Stretch;
-                    buttons[i, j].Click += new EventHandler(btn_Click);
-                    this.Controls.Add(buttons[i, j]);*/
-                    Console.WriteLine("Image " + ID);
+
+                    win1[i, j] = 1;
                 }
         }
 
@@ -59,17 +62,35 @@ namespace Safe2
             int i = this.PointToClient(Cursor.Position).X / 50;
             int countI;
             int countJ;
-            for (countI = 0; countI < (int)numericUpDown1.Value; countI++)
+            flip(buttons[i, j]);
+            /*for (countI = 0; countI < fieldSize; countI++)
             {
                 flip(buttons[countI, j]);
-                Console.WriteLine("Flipped");
             }
-            for (countJ =0; countJ < (int)numericUpDown1.Value; countJ++)
+            for (countJ =0; countJ < fieldSize; countJ++)
             {
                 if(countJ!=j)
                 flip(buttons[i, countJ]);
-                Console.WriteLine("Flipped");
+            } */
+
+            for (i = 0; i < fieldSize; i++)
+                for (j = 0; j < fieldSize; j++)
+                {
+                    current[i, j] = (int)buttons[i, j].Tag;
+                }
+            j = 0;
+            for (i = 0; i < fieldSize; i++)
+            {
+                for (j = 0; j < fieldSize; j++)
+                {
+
+                    Console.Write("{0,3}", current[j, i]);           
+                }
+                Console.WriteLine();
             }
+            Console.WriteLine();
+
+            won = checkWin();
 
         }
 
@@ -80,10 +101,43 @@ namespace Safe2
             btn.Tag = newID;
         }
 
+        private bool checkWin()
+        {
+            //bool check1 = Enumerable.SequenceEqual(current, win0);
+            bool check0 = false;
+           /* for (int i = 0; i < fieldSize; i++)
+                for (int j = 0; j < fieldSize; j++)
+                {
+                    if (current[i, j] == 0)
+                        win0 = true;
+                    else
+                    {
+                        win0 = false;
+                        break;
+                    }
+                }
+            for (int i = 0; i < fieldSize; i++)
+                for (int j = 0; j < fieldSize; j++)
+                {
+                    if (current[i, j] == 1)
+                        win1 = true;
+                    else
+                    {
+                        win1 = false;
+                        break;
+                    }
+                } */
+            if (win1 || win0)
+            {
+                MessageBox.Show("WON! win1 " +win1 + "win0 " + win0);
+                return true;
+            }
+            else
+                return false;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            levers[0] = Image.FromFile("Resources/Vert.png");
-            levers[1] = Image.FromFile("Resources/Hor.png");
 
         }
 
@@ -91,8 +145,8 @@ namespace Safe2
         {
             if (!started)
             {
-                Console.WriteLine("Сторона: " + (int)numericUpDown1.Value);
-                CreateButtons((int)numericUpDown1.Value);
+                Console.WriteLine("Сторона: " + fieldSize);
+                CreateButtons();
                 started = true;
                 btnStart3.Enabled = false;
             }
@@ -103,13 +157,5 @@ namespace Safe2
 
         }
 
-        void flipper(int fieldSize)
-        {
-            for (int i = 0; i < fieldSize; i++)
-                for (int j = 0; j < fieldSize; j++)
-                {
-                  //  flip();
-                }
-        }
     }
 }
