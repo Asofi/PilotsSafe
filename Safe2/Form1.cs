a751940b1e26ad223e1f141a361eae538b01293f
Asofi <asofi94@gmail.com>
2016-08-05 18:02:53 +0300

    Добавлены комментарии.

--- Safe2/Form1.cs
+++ Safe2/Form1.cs
@@ -30,6 +30,7 @@ namespace Safe2
             levers[0] = Image.FromFile("Resources/Hor.png");
         }
 
+        //Функция генерации матрицы
         private void CreateButtons()
         {
             fieldSize = (int)numericUpDown1.Value;
@@ -68,65 +69,65 @@ namespace Safe2
                             isEven = false;
                     }
 
-                    //Создается последняя строка на основании четности каждого столба
-                    for (int i = 0; i < fieldSize; i++)
+                //Создается последняя строка на основании четности каждого столба
+                for (int i = 0; i < fieldSize; i++)
+                {
+                    columnCount = 0;
+                    for (int j = 0; j < fieldSize - 1; j++)
                     {
-                        columnCount = 0;
-                        for (int j = 0; j < fieldSize - 1; j++)
-                        {
-                            if (ID[i, j] == 1)
-                                columnCount += 1;
-                        }
-                        if (columnCount % 2 == 0)
-                        {
-                            if (isEven)
-                                ID[i, fieldSize - 1] = 0;
-                            else
-                                ID[i, fieldSize - 1] = 1;
-                            buttons[i, fieldSize - 1].Tag = ID[i, fieldSize - 1];
-                            buttons[i, fieldSize - 1].BackgroundImage = levers[ID[i, fieldSize - 1]];
-                        }
-                        else
-                        {
-                            if (isEven)
-                                ID[i, fieldSize - 1] = 1;
-                            else
-                                ID[i, fieldSize - 1] = 0;
-                            buttons[i, fieldSize - 1].Tag = ID[i, fieldSize - 1];
-                            buttons[i, fieldSize - 1].BackgroundImage = levers[ID[i, fieldSize - 1]];
-                        }
+                        if (ID[i, j] == 1)
+                            columnCount += 1;
                     }
-
-                    ////Создается последний столбец на основании четности каждой строки
-                    for (int j = 0; j < fieldSize; j++)
+                    if (columnCount % 2 == 0)
                     {
-                        columnCount = 0;
-                        for (int i = 0; i < fieldSize - 1; i++)
-                        {
-                            if (ID[i, j] == 1)
-                                columnCount += 1;
-                        }
-                        if (columnCount % 2 == 0)
-                        {
-                            if (isEven)
-                                ID[fieldSize - 1, j] = 0;
-                            else
-                                ID[fieldSize - 1, j] = 1;
-                            buttons[fieldSize - 1, j].Tag = ID[fieldSize - 1, j];
-                            buttons[fieldSize - 1, j].BackgroundImage = levers[ID[fieldSize - 1, j]];
-                        }
+                        if (isEven)
+                            ID[i, fieldSize - 1] = 0;
                         else
-                        {
-                            if (isEven)
-                                ID[fieldSize - 1, j] = 1;
-                            else
-                                ID[fieldSize - 1, j] = 0;
-                            buttons[fieldSize - 1, j].Tag = ID[fieldSize - 1, j];
-                            buttons[fieldSize - 1, j].BackgroundImage = levers[ID[fieldSize - 1, j]];
-                        }
+                            ID[i, fieldSize - 1] = 1;
+                        buttons[i, fieldSize - 1].Tag = ID[i, fieldSize - 1];
+                        buttons[i, fieldSize - 1].BackgroundImage = levers[ID[i, fieldSize - 1]];
                     }
-
+                    else
+                    {
+                        if (isEven)
+                            ID[i, fieldSize - 1] = 1;
+                        else
+                            ID[i, fieldSize - 1] = 0;
+                        buttons[i, fieldSize - 1].Tag = ID[i, fieldSize - 1];
+                        buttons[i, fieldSize - 1].BackgroundImage = levers[ID[i, fieldSize - 1]];
+                    }
                 }
+
+                ////Создается последний столбец на основании четности каждой строки
+                for (int j = 0; j < fieldSize; j++)
+                {
+                    columnCount = 0;
+                    for (int i = 0; i < fieldSize - 1; i++)
+                    {
+                        if (ID[i, j] == 1)
+                            columnCount += 1;
+                    }
+                    if (columnCount % 2 == 0)
+                    {
+                        if (isEven)
+                            ID[fieldSize - 1, j] = 0;
+                        else
+                            ID[fieldSize - 1, j] = 1;
+                        buttons[fieldSize - 1, j].Tag = ID[fieldSize - 1, j];
+                        buttons[fieldSize - 1, j].BackgroundImage = levers[ID[fieldSize - 1, j]];
+                    }
+                    else
+                    {
+                        if (isEven)
+                            ID[fieldSize - 1, j] = 1;
+                        else
+                            ID[fieldSize - 1, j] = 0;
+                        buttons[fieldSize - 1, j].Tag = ID[fieldSize - 1, j];
+                        buttons[fieldSize - 1, j].BackgroundImage = levers[ID[fieldSize - 1, j]];
+                    }
+                }
+
+            }
             else
             {
                 for (int i = 0; i < fieldSize; i++)
@@ -137,26 +138,26 @@ namespace Safe2
                         buttons[i, j].BackgroundImage = levers[ID[i, j]];
                     }
             }
-                
+
 
         }
 
-
+        //Поворот крестом при клике мыши
         void btn_MouseClick(object sender, MouseEventArgs e)
         {
             int j = this.PointToClient(Cursor.Position).Y / 50;
             int i = this.PointToClient(Cursor.Position).X / 50;
 
-                for (countI = 0; countI < fieldSize; countI++)
-                {
-                    flip(buttons[countI, j]);
-                }
-                for (countJ = 0; countJ < fieldSize; countJ++)
-                {
-                    if (countJ != j)
-                        flip(buttons[i, countJ]);
-                }
-            
+            for (countI = 0; countI < fieldSize; countI++)
+            {
+                flip(buttons[countI, j]);
+            }
+            for (countJ = 0; countJ < fieldSize; countJ++)
+            {
+                if (countJ != j)
+                    flip(buttons[i, countJ]);
+            }
+
             for (i = 0; i < fieldSize; i++)
             {
                 for (j = 0; j < fieldSize; j++)
@@ -169,13 +170,15 @@ namespace Safe2
 
         }
 
-        private void flip (Button btn)
+        //Функция смены ручки
+        private void flip(Button btn)
         {
             int newID = 1 - (int)btn.Tag;
             btn.BackgroundImage = levers[newID];
             btn.Tag = newID;
         }
 
+        //Проверка победы
         private bool checkWin()
         {
             int prevI = 0;
@@ -189,11 +192,11 @@ namespace Safe2
                     prevI = i;
                     prevJ = j;
                 }
-            }              
+            }
 
-                MessageBox.Show("WON!");
-                return true;
-   
+            MessageBox.Show("WON!");
+            return true;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -201,14 +204,14 @@ namespace Safe2
 
         }
 
-
+        //Кнопка старта
         private void btnStart3_Click(object sender, EventArgs e)
         {
             restart();
-            
+
         }
 
-
+        //Функция запуска
         private void restart()
         {
             if (buttons != null)
@@ -222,33 +225,8 @@ namespace Safe2
                 CreateButtons();
             }
             else
-                CreateButtons();  
+                CreateButtons();
         }
 
-        private bool isCorrect()
-        {
-            int[] lineCount = new int[fieldSize];
-            int[] columnCount = new int[fieldSize];
-            for (int i = 0; i < fieldSize; i++)
-                for (int j = 0; j < fieldSize; j++)
-                {
-                    if ((int)buttons[i, j].Tag == 1)
-                    {
-                        columnCount[i] += 1;
-                        lineCount[j] += 1;
-                    }
-                }
-
-            for (int i = 1; i < fieldSize; i++)
-                {
-                    if ((columnCount[i] % 2 != columnCount[i - 1] % 2) || (lineCount[i] % 2 != columnCount[i - 1] % 2))
-                    {
-                        Console.WriteLine("Нерешаемо");
-                        return false;
-                    }
-                }
-            Console.WriteLine("Решаемо");
-            return true;
-        }
     }
 }
